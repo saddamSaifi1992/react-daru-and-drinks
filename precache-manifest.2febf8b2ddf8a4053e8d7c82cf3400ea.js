7f99e4a91f0d0e8115a1bb1bfb12a2944afa320c
saddam <saddam.phpdeveloper@gmai.com>
2020-03-11 16:51:37 +0530

    Updates

---
+++ precache-manifest.2febf8b2ddf8a4053e8d7c82cf3400ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922e1ef3acc6464db7f8442bd532b52c",
+    "url": "/react-daru-and-drinks/index.html"
+  },
+  {
+    "revision": "952b4f88e36726128b0b",
+    "url": "/react-daru-and-drinks/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "47cf10a047ba8e2506b9",
+    "url": "/react-daru-and-drinks/static/css/main.53421bb5.chunk.css"
+  },
+  {
+    "revision": "952b4f88e36726128b0b",
+    "url": "/react-daru-and-drinks/static/js/2.691839e3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-daru-and-drinks/static/js/2.691839e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47cf10a047ba8e2506b9",
+    "url": "/react-daru-and-drinks/static/js/main.97f4ffa5.chunk.js"
+  },
+  {
+    "revision": "b5e20e1f7404ef840fa5",
+    "url": "/react-daru-and-drinks/static/js/runtime-main.7196890a.js"
+  }
+]);
